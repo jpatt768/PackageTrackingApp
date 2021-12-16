871d0e79aa756d66e1b6905cde4cddebd0a49547
Jayden Patterson <jpatt768@gmail.com>
2021-12-16 21:36:36 +0900

    Initial Commit

---
+++ PackageTrackingApp 1.0/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PackageTrackingApp 1.0
+//
+//  Created by Jayden Patterson on 2021/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ PackageTrackingApp 1.0Tests/PackageTrackingApp_1_0Tests.swift
@@ -0,0 +1,33 @@
+//
+//  PackageTrackingApp_1_0Tests.swift
+//  PackageTrackingApp 1.0Tests
+//
+//  Created by Jayden Patterson on 2021/12/16.
+//
+
+import XCTest
+@testable import PackageTrackingApp_1_0
+
+class PackageTrackingApp_1_0Tests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
